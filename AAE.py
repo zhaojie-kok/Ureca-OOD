77bbe3c1a78155ef482e5984d37c83317fb2bbba
chrispyscream <kok.zhaojie@gmail.com>
2021-02-01 23:04:19 +0800

    enable use cfg files

--- AAE.py
+++ AAE.py
@@ -1,20 +1,19 @@
 import torch
 import torch.nn as nn
-import torch.nn.Functional as F
-
 import copy
+import yaml
+# TODO: docstrings and train loop (can't use from toolbox)
 
-#TODO: docstrings and train loop (can't use from toolbox)
 
 class encoder(nn.Module):
-    def __init__(self, input_dim, latent_dim, n_classes, hidden_dim=1, n_layers=0, dropout_p=0.2, input_block=None, hidden_block=None, y_block=None, z_block=None):
+    def __init__(self, input_dim, latent_dim, n_classes, hidden_dim=1, n_layers=0, dropout_p=0.2, input_block=None, blocks=None, y_block=None, y_predictor=None, z_block=None):
         super(encoder, self).__init__()
         self.input_dim = input_dim
         self.latent_dim = latent_dim
         self.hidden_dim = hidden_dim
 
         # create structure
-        if (input_block == None):
+        if (input_block is None):
             self.input_block = nn.Sequential(
                 nn.Linear(input_dim, hidden_dim),
                 nn.ReLU(),
@@ -24,29 +23,37 @@ class encoder(nn.Module):
         else:
             self.input_block = input_block
 
-        blocks = nn.ModuleList()
-        if (hidden_block == None):
+        if (blocks is None):
+            blocks = nn.ModuleList()
             hidden_block = nn.Sequential(
                 nn.Linear(hidden_dim, hidden_dim),
                 nn.ReLU(),
                 nn.BatchNorm1d(hidden_dim),
                 nn.Dropout(dropout_p)
-            )        
+            )
+            for i in range(n_layers):
+                blocks.append(copy.deepcopy(hidden_block))
+            self.blocks = nn.Sequential(*blocks)
+        else:
+            self.blocks = blocks
 
-        for i in range(n_layers):
-            blocks.append(copy.deepcopy(hidden_block))
-        
-        self.blocks = nn.Sequential(*blocks)
-
-        if (y_block == None):
+        if (y_block is None):
             self.y_block = nn.Sequential(
-                nn.Linear(hidden_dim, n_classes),
+                nn.Linear(hidden_dim, latent_dim),
                 nn.Softmax()
             )
         else:
             self.y_block = y_block
 
-        if (z_block == None):
+        if (y_predictor is None):
+            self.y_predictor = nn.Sequential(
+                nn.Linear(latent_dim, n_classes),
+                nn.Softmax()
+            )
+        else:
+            self.y_predictor = y_predictor
+
+        if (z_block is None):
             self.z_block = nn.Sequential(
                 nn.Linear(hidden_dim, latent_dim)
             )
@@ -58,9 +65,10 @@ class encoder(nn.Module):
         x = self.blocks(x)
 
         z = self.z_block(x)
-        y_ = self.y_block(x)
+        _y = self.y_block(x)
+        y_ = self.y_predictor(_y)
 
-        return z, y_
+        return z, _y, y_
 
 
 class decoder(nn.Module):
@@ -71,7 +79,7 @@ class decoder(nn.Module):
                  n_layers=0,
                  dropout_p=0.5,
                  input_block=None,
-                 hidden_block=None,
+                 blocks=None,
                  output_block=None):
         super(decoder, self).__init__()
         self.latent_dim = latent_dim
@@ -79,7 +87,7 @@ class decoder(nn.Module):
         self.output_dim = output_dim
 
         # create structure
-        if (input_block == None):
+        if (input_block is None):
             self.input_block = nn.Sequential(
                 nn.Linear(latent_dim, hidden_dim),
                 nn.ReLU(),
@@ -89,20 +97,21 @@ class decoder(nn.Module):
         else:
             self.input_block = input_block
 
-        if (hidden_block == None):
+        if (blocks is None):
             hidden_block = nn.Sequential(
                 nn.Linear(hidden_dim, hidden_dim),
                 nn.ReLU(),
                 nn.Dropout(dropout_p),
                 nn.BatchNorm1d(hidden_dim)
             )
+            blocks = nn.ModuleList()
+            for i in range(n_layers):
+                blocks.append(copy.deepcopy(hidden_block))
+            self.blocks = nn.Sequential(*blocks)
+        else:
+            self.blocks = blocks
 
-        blocks = nn.ModuleList()
-        for i in range(n_layers):
-            blocks.append(copy.deepcopy(hidden_block))
-        self.blocks = nn.Sequential(*blocks)
-
-        if (output_block == None):
+        if (output_block is None):
             self.output_block = nn.Sequential(
                 nn.Linear(hidden_dim, output_dim),
                 nn.ReLU(),
@@ -117,55 +126,78 @@ class decoder(nn.Module):
 
 
 class discrim(nn.Module):
-    def __init__(self, input_dim, hidden_dim, n_layers, dropout=0.2):
+    def __init__(self,
+                 input_dim,
+                 hidden_dim,
+                 n_layers=1,
+                 dropout=0.2,
+                 input_block=None,
+                 blocks=None,
+                 output_block=None):
         super(discrim, self).__init__()
-        self.input_block = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.Dropout(dropout),
-            nn.ReLU()
-        )
 
-        blocks = nn.ModuleList()
-        for i in range(n_layers):
-            blocks.append(nn.Sequential(
-                nn.Linear(hidden_dim, hidden_dim),
+        if input_block is None:
+            self.input_block = nn.Sequential(
+                nn.Linear(input_dim, hidden_dim),
                 nn.Dropout(dropout),
                 nn.ReLU()
-            ))
-        self.blocks = nn.Sequential(*blocks)
-        
-        self.out_block = nn.Sequential(
-            nn.Linear(hidden_dim, 1),
-            nn.Sigmoid()
-        )
+            )
+        else:
+            self.input_block = input_block
+
+        if blocks is None:
+            blocks = nn.ModuleList()
+            for i in range(n_layers):
+                blocks.append(nn.Sequential(
+                    nn.Linear(hidden_dim, hidden_dim),
+                    nn.Dropout(dropout),
+                    nn.ReLU()
+                ))
+            self.blocks = nn.Sequential(*blocks)
+        else:
+            self.blocks = blocks
+
+        if output_block is None:
+            self.output_block = nn.Sequential(
+                nn.Linear(hidden_dim, 1),
+                nn.Sigmoid()
+            )
+        else:
+            self.output_block = output_block
 
     def forward(self, input):
         x = self.input_block(input)
         x = self.blocks(x)
-        return self.out_block(x)
+        return self.output_block(x)
 
 
 class rand_sampler():
-    def __init__(self, dim, mu=None, sig=None, mode='cat', prob_tensor=None):
+    def __init__(self, dim, mu=None, sig=None, mode='cat', prob_tensor=None, portion=None):
         """
         dim: dimensionality for continuous random variable/num classes for cat mode
         """
-        assert mode in ('cat', 'cont')
+        assert mode in ('cat', 'cont', 'multi')
         self.mode = mode
-        self.dim = dim
+        self.dim = list(dim)
 
-        if mode is 'cont':
+        if mode == 'cont':
             self.mu = mu
             self.sig = sig
-        elif mode is 'cat':
+
+        elif mode == 'cat':
             prob_tensor = torch.ones(dim)/dim if prob_tensor is None else prob_tensor
             self.sampler = torch.distributions.OneHotCategorical(prob_tensor)
-    
+
+        elif mode == 'multi':
+            self.portion = portion
+
     def sample(self, n_samples):
-        if self.mode is 'cont':
-            return self.mu + (torch.randn(n_samples, self.dim) * self.sig)
-        elif self.mode is 'cat':
-            return self.sampler.sample((n_samples,))
+        if self.mode == 'cont':
+            return self.mu + (torch.randn(n_samples, *self.dim) * self.sig)
+        if self.mode == 'cat':
+            return self.sampler.sample((n_samples, ))
+        if self.mode == 'multi':
+            return 1*(torch.rand(n_samples, *self.dim) < self.portion)
 
 
 class semi_sup_AAE(nn.Module):
@@ -179,11 +211,98 @@ class semi_sup_AAE(nn.Module):
         self.z_rand = z_rand
         self.y_rand = y_rand
 
+    def forward(self, input):
+        z, _y, y_ = self.enc(input)
+        x_ = self.dec(torch.cat((z, _y), dim=1))
+        return x_, z, y_
+
     def get_reconst(self, input):
-        z, y_ = self.enc(input)
-        x_ = self.dec(torch.cat((z, y_), dim=1))
+        z, _y, y_ = self.enc(input)
+        x_ = self.dec(torch.cat((z, _y), dim=1))
         return x_
 
     def get_latents(self, input):
-        z, y_ = self.enc(input)
-        return z, y_
+        z, _y, y_ = self.enc(input)
+        return z, _y, y_
+
+    def discriminate(self, input, mode):
+        assert mode in 'yz'
+        if mode is 'y':
+            return self.d_y(input)
+        elif mode is 'z':
+            return self.d_z(input)
+
+    def get_samples(self, n_samples):
+        return self.z_rand.sample(n_samples), self.y_rand.sample(n_samples)
+
+
+def eval_type(layer_name):
+    if layer_name[:3] != 'nn.':
+        layer_name = 'nn.' + layer_name
+    return eval(layer_name)
+
+
+def construct_params(param_dict, cfg_dict):
+    for blk in cfg_dict.keys():
+        if blk == 'params':
+            continue
+
+        block = nn.ModuleList()
+        for layer in cfg_dict[blk]:
+            layer_type = eval_type(layer['type'])
+            block.append(layer_type(**layer['params']))
+
+            if layer.get('nlinear') is not None:
+                nlinear = layer['nlinear']
+                nlinear_type = eval_type(nlinear['type'])
+                block.append(nlinear_type(**nlinear['params']))
+
+            if layer.get('dropout') is not None:
+                block.append(nn.Dropout(**layer['dropout']))
+
+            if layer.get('norm') is not None:
+                norm = layer['norm']
+                norm_type = eval_type(norm['type'])
+                block.append(norm_type(**norm['params']))
+        block = nn.Sequential(*block)
+        param_dict[blk] = block
+    return param_dict
+
+
+def construct_model(cfg_path):
+    with open(cfg_path) as f:
+        cfg = yaml.load(f)
+        f.close()
+
+    # encoder
+    enc_cfg = cfg['enc']
+    enc_params = enc_cfg['params']
+    enc_params = construct_params(enc_params, enc_cfg)
+    enc = encoder(**enc_params)
+
+    # decoder
+    dec_cfg = cfg['dec']
+    dec_params = dec_cfg['params']
+    dec_params = construct_params(dec_params, dec_cfg)
+    dec = decoder(**dec_params)
+
+    # discriminator z
+    dz_cfg = cfg['disc_z']
+    dz_params = dz_cfg['params']
+    dz_params = construct_params(dz_params, dz_cfg)
+    d_z = discrim(**dz_params)
+
+    # discriminator y
+    dy_cfg = cfg['disc_y']
+    dy_params = dy_cfg['params']
+    dy_params = construct_params(dy_params, dy_cfg)
+    d_y = discrim(**dy_params)
+
+    # z sampler
+    z_rand = rand_sampler(**cfg['z_rand']['params'])
+
+    # y sampler
+    y_rand = rand_sampler(**cfg['y_rand']['params'])
+
+    return semi_sup_AAE(enc, dec, d_z, d_y, enc_params['n_classes'], z_rand,
+                        y_rand)
--- AAE_train.py
+++ AAE_train.py
@@ -7,6 +7,7 @@ import numpy as np
 import time
 import pickle
 
+
 def make_step(loss, o, s, params):
     loss.backward(retain_graph=True)
     nn.utils.clip_grad_norm_(params, 2.0)
@@ -14,10 +15,15 @@ def make_step(loss, o, s, params):
     if s is not None:
         s.step()
 
-def find_acc(true_probs, true_labs, false_probs=None, false_labs=None, threshold=0.5):
+
+def find_acc(true_probs,
+             true_labs,
+             false_probs=None,
+             false_labs=None,
+             threshold=0.5):
     if threshold < 0:
         assert len(true_probs) == len(true_labs)
-        preds = true_probs.argmax(dim = 1)
+        preds = true_probs.argmax(dim=1)
         return (torch.sum(preds == true_labs)/len(true_labs)).item()
 
     if true_labs.shape != true_probs.shape:
@@ -26,11 +32,11 @@ def find_acc(true_probs, true_labs, false_probs=None, false_labs=None, threshold
         for i in range(true_probs.size(0)):
             labs[i][true_labs[i]] = 1
         true_labs = labs
-    true_acc = ((true_probs>threshold)*1) == true_labs
+    true_acc = ((true_probs > threshold)*1) == true_labs
     true_acc = torch.sum(true_acc)/true_acc.nelement()
 
     false_acc = None
-    if false_probs != None:
+    if false_probs is not None:
         if false_labs.shape != false_probs.shape:
             assert len(false_labs) == false_probs.size(0)
             labs = torch.zeros_like(false_probs)
@@ -38,18 +44,27 @@ def find_acc(true_probs, true_labs, false_probs=None, false_labs=None, threshold
                 labs[i][false_labs[i]] = 1
             false_labs = labs
             print(false_labs)
-        false_acc = ((false_probs>threshold)*1) == false_labs
+        false_acc = ((false_probs > threshold)*1) == false_labs
         false_acc = torch.sum(false_acc)/false_acc.nelement()
 
         return true_acc.item(), false_acc.item()
-    
+
     return true_acc.item()
+
 
 def evaluate(model, val_dl, device):
     model.eval()
 
-    val_metrics = {'reconst':[], 'disc_y':[], 'disc_z':[], 'enc':[], 'y_':[],
-                   'z_d_acc':[], 'y_d_acc':[], 'y_acc':[]}
+    val_metrics = {
+        'reconst': [],
+        'disc_y': [],
+        'disc_z': [],
+        'enc': [],
+        'y_': [],
+        'z_d_acc': [],
+        'y_d_acc': [],
+        'y_acc': []
+    }
 
     for _, (x_unlab, xs, ys) in enumerate(val_dl):
         x_unlab = x_unlab.to(device)
@@ -58,28 +73,31 @@ def evaluate(model, val_dl, device):
 
         with torch.no_grad():
             x_, z, y_ = model(x_unlab)
-        
+
         # reconst phase
         reconst_loss = F.mse_loss(x_, x_unlab, reduction='sum')/x_unlab.size(0)
         val_metrics['reconst'].append(reconst_loss.item())
 
         # regularisation (discrim)
-        z_sample, y_sample = (s.to(device) for s in model.get_samples(xs.size(0)))
+        z_sample, y_sample = (s.to(device)
+                              for s in model.get_samples(xs.size(0)))
 
         with torch.no_grad():
             p_real_z = model.discriminate(z_sample, 'z')
             p_fake_z = model.discriminate(z, 'z')
-            p_real_y = model.discriminate(y_sample, 'y') # TODO: try with actual y
+            p_real_y = model.discriminate(y_sample, 'y')
             p_fake_y = model.discriminate(y_, 'y')
 
         d_loss_z = torch.mean(-torch.log(p_real_z) - torch.log(1-p_fake_z))
         d_loss_y = torch.mean(-torch.log(p_real_y) - torch.log(1-p_fake_y))
         val_metrics['disc_z'].append(d_loss_z.item())
         val_metrics['disc_y'].append(d_loss_y.item())
-        val_metrics['z_d_acc'].append(find_acc(p_real_z, torch.ones_like(p_real_z),
-                                             p_fake_z, torch.zeros_like(p_fake_z)))
-        val_metrics['y_d_acc'].append(find_acc(p_real_y, torch.ones_like(p_real_y),
-                                             p_fake_y, torch.zeros_like(p_fake_y)))
+        val_metrics['z_d_acc'].append(
+            find_acc(p_real_z, torch.ones_like(p_real_z), p_fake_z,
+                     torch.zeros_like(p_fake_z)))
+        val_metrics['y_d_acc'].append(
+            find_acc(p_real_y, torch.ones_like(p_real_y), p_fake_y,
+                     torch.zeros_like(p_fake_y)))
 
         # regularisation (generator/encoder)
         g_loss = torch.mean(-torch.log(p_fake_z) - torch.log(p_fake_y))
@@ -94,13 +112,31 @@ def evaluate(model, val_dl, device):
 
     return val_metrics
 
-def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, save=True, cache_dir=None, val_dl=None, start_epoch=1):
-    val_flag = (val_dl != None)
+
+def train(model,
+          train_dl,
+          num_epochs,
+          device,
+          optim_sched,
+          verbose_freq=50,
+          save=True,
+          cache_dir=None,
+          val_dl=None,
+          start_epoch=1):
+    val_flag = (val_dl is not None)
     model.to(device)
 
-    batch_history = {'reconst':[], 'disc_y':[], 'disc_z':[], 'enc':[], 'y_':[],
-                     'z_d_acc':[], 'y_d_acc':[], 'y_acc':[]}
-    epoch_history = {'train':dict(batch_history), 'val':dict(batch_history)}
+    batch_history = {
+        'reconst': [],
+        'disc_y': [],
+        'disc_z': [],
+        'enc': [],
+        'y_': [],
+        'z_d_acc': [],
+        'y_d_acc': [],
+        'y_acc': []
+    }
+    epoch_history = {'train': dict(batch_history), 'val': dict(batch_history)}
 
     start_time = time.time()
     for epoch in range(start_epoch, start_epoch + num_epochs):
@@ -116,18 +152,21 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
 
             # reconst phase
             x_ = model.get_reconst(x_unlab)
-            reconst_loss = F.mse_loss(x_, x_unlab, reduction='sum')/x_unlab.size(0)
+            reconst_loss = F.mse_loss(x_, x_unlab,
+                                      reduction='sum') / x_unlab.size(0)
             batch_history['reconst'].append(reconst_loss.item())
-            make_step(reconst_loss, *optim_sched['reconst'], model.parameters())
+            make_step(reconst_loss, *optim_sched['reconst'],
+                      model.parameters())
 
             # regularisation (discrim)
-            model.enc.eval() # since information shouldnt be restricted through encoder at this stage
-            z_sample, y_sample = (s.to(device) for s in model.get_samples(xs.size(0)))
+            model.enc.eval()  # since information shouldnt be restricted through encoder at this stage
+            z_sample, y_sample = (s.to(device)
+                                  for s in model.get_samples(xs.size(0)))
             z, y_ = model.get_latents(x_unlab)
 
             p_real_z = model.discriminate(z_sample, 'z')
             p_fake_z = model.discriminate(z, 'z')
-            p_real_y = model.discriminate(y_sample, 'y') # TODO: try with actual y
+            p_real_y = model.discriminate(y_sample, 'y')
             p_fake_y = model.discriminate(y_, 'y')
 
             d_loss_z = torch.mean(-torch.log(p_real_z) - torch.log(1-p_fake_z))
@@ -136,15 +175,17 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
             batch_history['disc_y'].append(d_loss_y.item())
             make_step(d_loss_z, *optim_sched['disc_z'], model.parameters())
             make_step(d_loss_y, *optim_sched['disc_y'], model.parameters())
-            
-            batch_history['z_d_acc'].append(find_acc(p_real_z, torch.ones_like(p_real_z),
-                                                p_fake_z, torch.zeros_like(p_fake_z)))
-            batch_history['y_d_acc'].append(find_acc(p_real_y, torch.ones_like(p_real_y),
-                                                p_fake_y, torch.zeros_like(p_fake_y)))
+
+            batch_history['z_d_acc'].append(
+                find_acc(p_real_z, torch.ones_like(p_real_z), p_fake_z,
+                         torch.zeros_like(p_fake_z)))
+            batch_history['y_d_acc'].append(
+                find_acc(p_real_y, torch.ones_like(p_real_y), p_fake_y,
+                         torch.zeros_like(p_fake_y)))
 
             # regularisation (generator/encoder)
             model.enc.train()
-            z, y_ = model.get_latents(x_unlab) # TODO: try without generating new latents
+            z, y_ = model.get_latents(x_unlab)
             p_fake_z = model.discriminate(z, 'z')
             p_fake_y = model.discriminate(y_, 'y')
             g_loss = torch.mean(-torch.log(p_fake_z) - torch.log(p_fake_y))*2
@@ -168,7 +209,7 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
                     except:
                         print(f"{metric:^15} : {batch_mean}")
                 print('-'*100)
-        
+
         print(f"Epoch {epoch} Train:")
         for metric, record in batch_history.items():
             epoch_mean = np.mean(record[-len(train_dl):], axis=0)
@@ -177,7 +218,7 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
                 print(f"|{metric:^15}| : {epoch_mean:.6f}")
             except:
                 print(f"|{metric:^15}| : {epoch_mean}")
-        
+
         val_metrics = None
         if val_flag:
             print(f"Epoch {epoch} Val:")
@@ -189,14 +230,17 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
                     print(f"|{metric:^15}| : {val_mean:.6f}")
                 except:
                     print(f"|{metric:^15}| : {val_mean}")
-        
+
         if save:
             save_state = {
-                'state_dict':model.state_dict(),
-                'epoch':epoch,
-                'optim_state_dicts':{name:optim.state_dict() for name, (optim, sched) in optim_sched.items()}
+                'state_dict': model.state_dict(),
+                'epoch': epoch,
+                'optim_state_dicts': {
+                    name: optim.state_dict()
+                    for name, (optim, sched) in optim_sched.items()
+                }
             }
-            torch.save(save_state, cache_dir  + f'/savestates/AAE_MNIST_{epoch}')
+            torch.save(save_state, cache_dir + f'/savestates/AAE_MNIST_{epoch}')
             with open(cache_dir + f'/records/AAE_MNIST_{epoch}.pkl', 'wb') as stat_f:
                 pickle.dump((epoch_history, batch_history), stat_f)
                 stat_f.close()
@@ -205,4 +249,4 @@ def train(model, train_dl, num_epochs, device, optim_sched, verbose_freq=50, sav
 
     total_time = time.time()-start_time
     print(f'Total time taken: {total_time:.2f}')
-    return epoch_history, batch_history        
+    return epoch_history, batch_history
